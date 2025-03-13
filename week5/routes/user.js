4d500daa329689a5a9461030f8c5a763f5644c7d
rubyLin <ruby.lin@urmart.com>
2025-03-13 10:18:34 +0800

    fix : user

--- week5/routes/user.js
+++ week5/routes/user.js
@@ -4,7 +4,6 @@ const router = express.Router();
 const { dataSource } = require("../db/data-source");
 const config = require("../config/index");
 
-const StatusCode = require("../constant/StatusCode");
 const logger = require("../utils/logger")("user");
 
 const auth = require("../middlewares/auth")({
@@ -19,9 +18,6 @@ const {
   getUserProfile,
   updateUserProfile,
 } = require("../controllers/user");
-
-const { errorResponse, successResponse } = require("../utils/response");
-const { validateString, validatedInteger } = require("../utils/validation");
 
 router.post("/signup", userSignup);
 
